656e227a02cb6becaad6e03dc9131f7835a70b8c
Davi Afonso <daafonso533@gmail.com>
2024-09-23 15:44:13 -0300

    23/09
    Criação jogo da forca

---
+++ static/js/pergunta.js
@@ -0,0 +1,26 @@
+const palavras = [
+   "robert langdong",
+    "louvre",
+    "sophie neveu",
+    "opus dei",
+    "teabing",
+    "da vinci",
+    "priorado de sião",
+    "santo graal",
+    "silas",
+    "o código da vinci"
+
+]
+
+const dicas = [
+    "Simbologista de Harvard que investiga o assassinato",
+    "Nome do museu onde Jacques Saunière é assassinado",
+    "Nome da criptóloga que ajuda Langdon na investigação",
+    "Organização religiosa que tenta proteger o segredo do Graal",
+    "Historiador obcecado pelo Graal",
+    "Artista famoso que tem suas obras associadas a pistas sobre o Santo Graal",
+    "Nome da sociedade secreta que protege o Santo Graal",
+    "Objeto histórico/religioso que é o foco da trama",
+    "Nome do monge que comete os crimes a mando da Opus Dei",
+    "Nome do livro que todos esses eventos acontecem",
+]
